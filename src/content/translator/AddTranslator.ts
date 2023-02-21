8c0ff7cc3b547659a0f3933f31555717b200167e
pleahmacaka <pleahmacaka@gmail.com>
2023-02-21 10:15:46 +0900

    refactor: rename func

--- src/content/translator/AddTranslator.ts
+++ src/content/translator/AddTranslator.ts
@@ -2,36 +2,45 @@ import { postData } from "../../background/Translator";
 import { TranslatorConfig } from "./TranslatorConfig"
 
 function addTranslator() {
+	// opened options
 	const optionsContent = document.querySelector(".options-content")
 	optionsContent?.setAttribute("style", "top: -50px")
 
+	// toggle button
 	const toggleTranslator = document.createElement("a")
 	toggleTranslator.id = "option_toggle_translator"
 	toggleTranslator.text = "Toggle Translator"
 
+	// button icon
 	const translatorIcon =
 		"https://cdn.discordapp.com/attachments/1072301775606001744/1072301837304221716/translator.png"
 	toggleTranslator.innerHTML = `<img src="${translatorIcon}" width="20" height="20" alt="translator">Toggle Translator`
 
+	// add toggle button in options
 	optionsContent?.appendChild(toggleTranslator)
 
+	// toggle to change config
 	document
 		.getElementById("option_toggle_translator")
 		?.addEventListener("click", () => {
 			TranslatorConfig.enabled = !TranslatorConfig.enabled
 			console.log("TavernAI Translator: " + TranslatorConfig.enabled)
-			if (TranslatorConfig.enabled) enableTranslatorTextarea()
-			else disableTranslatorTextarea()
+			if (TranslatorConfig.enabled) enableTranslator()
+			else disableTranslator()
 		})
 }
 
-function enableTranslatorTextarea() {
+function enableTranslator() {
+	// sendTextArea : existing message box (right)
+	// translatorTextArea : new message box for translate (left)
 	const sendTextarea = document.querySelector("#send_textarea")
 	const translatorTextarea = document.createElement("textarea")
+
+	// sendForm : existing form
 	const sendForm = document.getElementById("send_form")
 
 	sendTextarea?.setAttribute("style", "width: 50%; height: 89px; margin-left: 0;")
-	translatorTextarea.setAttribute("placeholder", "Message to translate");
+	translatorTextarea.setAttribute("placeholder", "Message to translate...");
 	translatorTextarea.id = "translator_textarea"
 	translatorTextarea.setAttribute("style", "width: 50%; height: 89px; margin-left: 0;")
 	sendTextarea?.parentNode?.insertBefore(translatorTextarea, sendTextarea)
@@ -59,7 +68,7 @@ function enableTranslatorTextarea() {
 /**
  * revert all css changes
  */
-function disableTranslatorTextarea() {
+function disableTranslator() {
 	const sendTextarea = document.querySelector("#send_textarea")
 	const translatorTextarea = document.querySelector("#translator_textarea")
 	const sendForm = document.getElementById("send_form")
@@ -72,6 +81,6 @@ function disableTranslatorTextarea() {
 }
 
 export function initTranslator() {
-	if (TranslatorConfig.enabled) enableTranslatorTextarea()
+	if (TranslatorConfig.enabled) enableTranslator()
 	addTranslator()
 }
